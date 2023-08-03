e2de92e26340557b25a629f99f9d3111d6a1a0d5
Philipp Rieser <philipp.rieser@gmail.com>
2023-08-03 22:51:52 +0200

    remove template files, implement react-router-dom, RouterError.tsx

--- phibry/src/main.tsx
+++ phibry/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RouterError from './RouterError.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <RouterError />,
+    },
+    {
+        path: "/vizirino",
+        element: <div>Vizirino</div>
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+);
