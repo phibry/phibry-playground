07a9de05cc6698f5edb295527fe1095a6881adbd
Philipp Rieser <philipp.rieser@gmail.com>
2023-08-04 14:16:07 +0200

    base path /phibry-playground/phibry

--- phibry/vite.config.ts
+++ phibry/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/phibry/",
+    base: "/phibry-playground/phibry/",
     plugins: [react()],
 })
