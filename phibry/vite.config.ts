05d1a0dee179d6b2aa3dcc63007e2429da14d967
Philipp Rieser <philipp.rieser@gmail.com>
2023-08-04 10:16:41 +0200

    path to assets

--- phibry/vite.config.ts
+++ phibry/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/phibry-playground/phibry",
+    base: "/phibry-playground/",
     plugins: [react()],
 })
